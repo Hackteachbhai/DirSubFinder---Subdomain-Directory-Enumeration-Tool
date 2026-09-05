b60cfb4c542574da7092e0ccabb29a03b9b22285
Vimal Bijalwan <161735776+Hackteachbhai@users.noreply.github.com>
2026-09-06 01:07:54 +0530

    Initialize dirsubfinder module with metadata

---
+++ dirsubfinder/__init__.py
@@ -0,0 +1,8 @@
+# dirsubfinder/__init__.py
+"""
+DirSubFinder - Professional Subdomain & Directory Enumeration Tool
+Made by Vimal Bijalwan
+"""
+
+__version__ = "1.0.0"
+__author__ = "Vimal Bijalwan"
