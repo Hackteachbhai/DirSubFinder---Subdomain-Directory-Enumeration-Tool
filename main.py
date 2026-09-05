8540438d0640def9873894452f27713fb504b93f
Vimal Bijalwan <161735776+Hackteachbhai@users.noreply.github.com>
2026-09-06 01:07:01 +0530

    Initialize main.py for DirSubFinder tool

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+#!/usr/bin/env python3
+"""
+DirSubFinder - Subdomain & Directory Enumeration Tool
+Made by Vimal Bijalwan
+"""
+
+from dirsubfinder.cli import main
+
+if __name__ == "__main__":
+    main()
